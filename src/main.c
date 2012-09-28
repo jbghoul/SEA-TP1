0f2e6a0b92d65a19da85a7151efb9782a8f5bf8f
Ghoul Jean-Baptiste <jbghoul@if219-07.insa-lyon.fr>
2012-09-28 09:49:45 +0200

    First commit - SEA

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+
+#define RETURN_SUCCESS 0
+#define RETURN_FAILURE 1
+
+int
+main(int argc, char *argv[])
+{
+    exit(EXIT_SUCCESS);
+}

dd5b3e46f78ead33125f0ad4c468581b5b2b1947
Jean-Baptiste Ghoul <jean-baptiste.ghoul@insa-lyon.fr>
2012-10-05 09:29:40 +0200

    Ajout test pile

---
+++ src/TestPile.c
@@ -0,0 +1,67 @@
+#include <setjmp.h>
+#include <stdio.h>
+
+static jmp_buf buf;
+
+
+int toto(int);
+int banane(int);
+
+int banane(int fin) {
+unsigned int x=0, y=0;
+asm ("movl %%esp, %0" "\n\t" "movl %%ebp, %1"
+  : "=r"(y),"=r"(x) 
+  :
+  );
+
+printf("banane \nbp: %ud \nsp: %ud\n",x,y);
+  if(fin) {
+  return 0;
+  }
+  else {
+  return toto(1);
+  }
+}
+
+
+int toto(int fin) {
+unsigned int x=0, y=0;
+asm ("movl %%esp, %0" "\n\t" "movl %%ebp, %1"
+  : "=r"(y),"=r"(x) 
+  :
+  );
+
+printf("toto \nbp: %ud \nsp: %ud\n",x,y);
+  if(fin) {
+  return 0;
+  }
+  else
+  {
+  return banane(0);
+}
+}
+
+
+int
+main(void)
+{
+unsigned int x=0, y=0;
+asm ("movl %%esp, %0" "\n\t" "movl %%ebp, %1"
+  : "=r"(y),"=r"(x) 
+  :
+  );
+
+printf("main \nbp: %ud \nsp: %ud\n",x,y);
+
+toto(0);
+
+asm ("movl %%esp, %0" "\n\t" "movl %%ebp, %1"
+  : "=r"(y),"=r"(x) 
+  :
+  );
+
+printf("main \nbp: %ud \nsp: %ud\n",x,y);
+
+return 0;
+
+}
